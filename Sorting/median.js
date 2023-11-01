9cbf3c995d8607b50959933e11eb4d234eb19a45
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-01 16:01:11 -0700

    Added function to verify if the array's number of elements is odd or even

--- Sorting/median.js
+++ Sorting/median.js
@@ -6,11 +6,23 @@ function sortedArray(array)
         return left - right;
     })
 
+    evenOrOdd(array);
+}
+
+function evenOrOdd(array)
+{
+    if (array.length % 2 == 0)
+    {
+        console.log("Even array");
+    }
+    else console.log("Odd array");
 }
 
 
 
 
-sortedArray([92, 98, 84, 76, 89, 99, 100]);
+
+
+sortedArray([92, 98, 84, 76, 89, 100]);
 console.log("--------------------------------");
 sortedArray([82, 98, 94, 88, 92, 100, 100])
