935ae4575dfc8e659d3559581b13fdbc6f59a82e
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-01 22:40:09 -0700

    Updated evenOrOdd function

--- Sorting/median.js
+++ Sorting/median.js
@@ -11,18 +11,13 @@ function sortedArray(array)
 
 function evenOrOdd(array)
 {
-    if (array.length % 2 == 0)
-    {
-        console.log("Even array");
-    }
-    else console.log("Odd array");
+    let modulus = array.length % 2;
+
+    let average = getAverage(array);
+    let median = getMedian(array, modulus);
 }
-
-
-
-
 
 
 sortedArray([92, 98, 84, 76, 89, 100]);
 console.log("--------------------------------");
-sortedArray([82, 98, 94, 88, 92, 100, 100])
+// sortedArray([82, 98, 94, 88, 92, 100, 100])
