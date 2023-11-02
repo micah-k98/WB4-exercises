7a124b9adbeea236aedbf3f2ba20d6b2ed1244b4
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-01 22:41:11 -0700

    Added getAverage function

--- Sorting/median.js
+++ Sorting/median.js
@@ -17,6 +17,19 @@ function evenOrOdd(array)
     let median = getMedian(array, modulus);
 }
 
+function getAverage(array)
+{
+    let length = array.length;
+    let totalScores = 0;
+
+    for (let i = 0; i < length; i++)
+    {
+        totalScores = totalScores + array[i];
+    }
+
+    return +(totalScores / length).toFixed(2);
+}
+
 
 sortedArray([92, 98, 84, 76, 89, 100]);
 console.log("--------------------------------");
